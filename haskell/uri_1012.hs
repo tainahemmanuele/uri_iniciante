bf7f6b314b44b4c759acd1aadd7ee0c733036108
tainahemmanuele <tainahemmanuele@gmail.com>
2019-02-16 11:23:54 -0300

    Questao 1012 resolvida em haskell

--- haskell/uri_1012.hs
+++ haskell/uri_1012.hs
@@ -1,23 +1,22 @@
 import Text.Printf
-import Data.List.Split
-calcula_triangulo :: Float -> Float -> Float
+calcula_triangulo :: Double -> Double -> Double
 calcula_triangulo a c = (a*c)/2
 
-calcula_circulo :: Float -> Float
-calcula_circulo c = 3.14159 * (c*c)
+calcula_circulo :: Double -> Double
+calcula_circulo c = 3.14159 *c*c
 
-calcula_trapezio :: Float -> Float -> Float -> Float
-calcula_trapezio a b c = ((a+b)*c) / 2
+calcula_trapezio :: Double -> Double -> Double -> Double
+calcula_trapezio a b c = ( c * (a+b)) / 2
 
-calcula_quadrado :: Float -> Float
+calcula_quadrado :: Double -> Double
 calcula_quadrado b = b*b
 
-calcula_retangulo :: Float -> Float -> Float
+calcula_retangulo :: Double -> Double -> Double
 calcula_retangulo a b = a*b
 
 main = do
    values <- getLine
-   let list = splitOn " " values
+   let list = words values
    let a = list !! 0
    let b = list !! 1
    let c = list !! 2
